a3590c53d5dd92b311c4196cfe7c942b072c5a9d
Anton Menchaca <menchacanton@gmail.com>
2020-04-21 23:20:45 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -2,7 +2,7 @@ function basicTeenager(age >=13 || <=19);
 if (true) {
 var basicTeenager >=13 || <=19;
 }
-if (age >=13 || <=19) {console.log("You are a teenager!")
+if (age >=13 || <=19) {return("You are a teenager!")
 
 }
 if
